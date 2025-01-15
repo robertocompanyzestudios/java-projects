af90ce88c7bc96197cc8b326d8fc13c89bc00aa7
robertocompanyzestudios <robertocompanyzestudios@gmail.com>
2025-01-15 12:32:52 +0100

    commit 2025-01-15_12-32

---
+++ programacionInstituto/src/programacionInstituto/EstudiantePrueba.java
@@ -0,0 +1,12 @@
+package programacionInstituto;
+
+public class EstudiantePrueba {
+	private static Estudiante[] estudiantes;
+	
+	public static void crearEstudiantes() {
+		estudiantes[0] = 
+	}
+	
+	
+
+}
